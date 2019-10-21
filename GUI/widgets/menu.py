35eb95c91e32033b0d737ccf6662139dab50553e
facuser <facuser@et-eta408-l10.ad.calstatela.edu>
2019-10-21 12:58:05 -0700

    Adding new version to detect Car, Truck, Bus objects

---
+++ GUI/widgets/menu.py
@@ -0,0 +1,33 @@
+# Copyright (c) Data Science Research Lab at California State University Los Angeles (CSULA), and City of Los Angeles ITA
+# Distributed under the terms of the Apache 2.0 License
+# www.calstatela.edu/research/data-science
+# Designed and developed by:
+# Data Science Research Lab
+# California State University Los Angeles
+# Dr. Mohammad Pourhomayoun
+# Mohammad Vahedi
+# Haiyan Wang
+
+from tkinter import *
+
+class App_Menu(Menu):
+    def __init__(self,parent,controller):
+
+        parent.option_add('*tearOff', FALSE)
+        menubar = Menu(parent)
+        parent['menu'] = menubar
+
+        menubar = Menu(parent)
+        parent.config(menu= menubar)
+        menu_file = Menu(menubar)
+        menu_tools = Menu(menubar)
+        menubar.add_cascade(menu=menu_file, label='File')
+        menubar.add_cascade(menu=menu_tools, label='Tools')
+
+        menu_file.add_command(label='Open...', command=controller.open_file)
+        menu_file.add_separator()
+        menu_file.add_command(label='Exit', command=parent.quit)
+
+        menu_tools.add_command(label='Add AOI', command=controller.add_new_aoi)
+
+
---
+++ development/bounding_box/detected_object.py
@@ -0,0 +1,46 @@
+# Copyright (c) Data Science Research Lab at California State University Los Angeles (CSULA), and City of Los Angeles ITA
+# Distributed under the terms of the Apache 2.0 License
+# www.calstatela.edu/research/data-science
+# Designed and developed by:
+# Data Science Research Lab
+# California State University Los Angeles
+# Dr. Mohammad Pourhomayoun
+# Mohammad Vahedi
+# Haiyan Wang
+
+class DetectedObject:
+
+	def __init__(self,box):
+
+		self.box = box
+		(left, right, top, bot, mess, max_index, confidence) = box
+		self.left = left
+		self.right = right
+		self.top = top
+		self.bot = bot
+		self.mess = mess
+		self.max_indx = max_index
+		self.confidence = confidence
+		self.center_x = 0
+		self.center_y = 0
+		self.center = self.getObjectCenteralPointArray()	
+
+	def getObjectCenteralPointArray(self):
+		cx = self.left + int((self.right-self.left)/2)
+		self.center_x = cx
+		cy = self.top + int((self.bot-self.top)/2)
+		self.center_y = cy
+		return [cx,cy]
+
+	def getWidth(self):
+		return self.right-self.left
+
+	def getHeigth(self):
+		return self.bot-self.top
+
+
+
+	
+
+	
+
